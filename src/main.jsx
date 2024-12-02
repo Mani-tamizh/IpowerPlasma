5b3d77b759c2ed74d00d0e4576e1081200bd58ca
Mani-tamizh <152505407+Mani-tamizh@users.noreply.github.com>
2024-12-02 17:49:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
-  <HelmetProvider>  
     <StrictMode>
       <App />
-    </StrictMode>,
-  </HelmetProvider>
+    </StrictMode>
 )
